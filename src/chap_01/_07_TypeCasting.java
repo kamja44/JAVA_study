46e901207045cf6bbe2e8ed288a06ad8b66acd72
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-12-25 23:54:35 +0900

    1. TypeCasting

---
+++ src/chap_01/_07_TypeCasting.java
@@ -0,0 +1,11 @@
+package chap_01;
+
+public class _07_TypeCasting {
+    public static void main(String[] args) {
+        // 형변환 TypeCasting
+        // 정수 -> 실수
+        // 실수 -> 정수
+        int score = 93 = 98.8;
+
+    }
+}
