2e611cfc95e5268688c4487e3b578f604f11410b
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-12-30 23:25:21 +0900

    1. Type Converted part 1

--- src/chap_01/_07_TypeCasting.java
+++ src/chap_01/_07_TypeCasting.java
@@ -5,7 +5,33 @@ public class _07_TypeCasting {
         // 형변환 TypeCasting
         // 정수 -> 실수
         // 실수 -> 정수
-        int score = 93 = 98.8;
+//        int score = 93 = 98.8;
+        // int to float, double
+        int score = 93;
+        System.out.println(score); // 93
+        System.out.println((float)score); // 93.0
+        System.out.println((double) score); // 93.0
+
+        // float, double to int
+        float score_f = 93.3F;
+        double score_d = 98.8;
+        System.out.println((int)score_f); // 93
+        System.out.println((int)score_d); // 98
+
+        // 정수 + 실수 연산
+        score = 93 + (int)98.8; // 93 + 98 = 191
+        System.out.println(score);
+
+        // 정수 + 실수 <- 데이터의 크기가 큰 실수타입으로 정수형이 실수형으로 자동 형변환
+        score_d = 93 + 98.8; // 93.0 + 98.8
+        System.out.println(score_d); // 191.8
+
+        // 변수에 형변환된 데이터 집어넣기
+        // int -> long > float -> double (자동 형변환)
+        double convertedScoreDouble = score; // 191 -> 191.0
+
+        // double -> float -> long -> int (수동 형변환)
+        int convertedScoreInt = (int) score_d; // 191.8 -> 191
 
     }
 }
